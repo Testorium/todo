7f35f26267ea30f037877b269c4b7d689feb3e59
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2025-06-06 16:14:39 +0500

    feat: add user service, for the sake of simplicity there are not all methods included

---
+++ backend/features/user/service.py
@@ -0,0 +1,94 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import select
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+from shared.utils.hashing import hash_password
+
+from backend.shared.exceptions.database import (
+    AlreadyExistsError,
+    DatabaseError,
+    NotFoundError,
+)
+
+from .model import User
+from .schemas import UserCreate, UserUpdate
+
+
+class UserService:
+    def __init__(self, session: "AsyncSession"):
+        self._session = session
+
+    async def create_user(self, user_data: UserCreate) -> User:
+        existing_user = await self.get_user_by_username(user_data.username)
+
+        if existing_user:
+            raise AlreadyExistsError("User already exists.")
+
+        hashed_pwd = hash_password(user_data.password)
+
+        user = User(
+            username=user_data.username,
+            hashed_password=hashed_pwd,
+            first_name=user_data.first_name,
+            last_name=user_data.last_name,
+        )
+
+        try:
+            self._session.add(user)
+            await self._session.commit()
+            await self._session.refresh(user)
+
+        except Exception:
+            await self._session.rollback()
+            raise DatabaseError("Failed to create new user.")
+
+        return user
+
+    async def get_user_by_username(self, username: str) -> User | None:
+        statement = select(User).where(User.username == username)
+
+        try:
+            result = await self._session.execute(statement)
+            user = result.scalar_one_or_none()
+
+        except Exception as e:
+            # logger.exception("Failed to retrieve user.")
+            raise DatabaseError("Failed to retrieve user.")
+
+        return user
+
+    async def get_user_by_id(self, user_id: int) -> User | None:
+        statement = select(User).where(User.id == user_id)
+
+        try:
+            result = await self._session.execute(statement)
+            user = result.scalar_one_or_none()
+
+        except Exception as e:
+            # logger.exception("Failed to retrieve user.")
+            raise DatabaseError("Failed to retrieve user.")
+
+        if not user:
+            raise NotFoundError("User not found.")
+
+        return user
+
+    async def update_user_by_id(self, new_data: UserUpdate, user_id: int) -> User:
+        user = self.get_user_by_id(user_id)
+
+        values = new_data.model_dump(exclude_none=True, exclude_unset=True)
+
+        for key, value in values.items():
+            setattr(user, key, value)
+
+        try:
+            await self._session.commit()
+            await self._session.refresh(user)
+            return user
+
+        except Exception as e:
+            # logger.exception("Failed to update user.")
+            raise DatabaseError("Failed to update user.")
