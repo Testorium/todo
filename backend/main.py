5902e98d5740186445eda9c726df2f92d3df1ed5
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2025-06-07 14:07:02 +0500

    feat: add main router to app and add all tables creation to lifespan events

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,20 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from shared.database import db_manager
+from shared.models import Base
+from shared.routers import main_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+
+    async with db_manager.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
     yield
     await db_manager.dispose()
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(main_router)
