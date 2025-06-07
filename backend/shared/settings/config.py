71c6980c7f839c8db332a1dc64e82f6a49b427ec
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2025-06-07 14:42:44 +0500

    feat: add gunicorn configurations

--- backend/shared/settings/config.py
+++ backend/shared/settings/config.py
@@ -66,3 +66,15 @@ class CookieConfig:
 
 
 cookie_config = CookieConfig()
+
+
+# --- Gunicorn Configuration ---
+class GunicornConfig:
+    host: str = "0.0.0.0"
+    port: int = 8000
+    timeout: int = 900
+    workers: int = 1
+    log_level: str = "info"
+
+
+gunicorn_config = GunicornConfig()
