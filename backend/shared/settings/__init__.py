8218edc92ce24a9e5989445ddfd653120144604c
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2025-06-07 14:43:11 +0500

    feat: add gunicorn config to init file

--- backend/shared/settings/__init__.py
+++ backend/shared/settings/__init__.py
@@ -1,1 +1,7 @@
-from .config import api_prefix_config, cookie_config, db_config, jwt_config
+from .config import (
+    api_prefix_config,
+    cookie_config,
+    db_config,
+    gunicorn_config,
+    jwt_config,
+)
