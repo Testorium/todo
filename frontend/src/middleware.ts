f1fdebeeda78521a79ee70066bc019a913f655b8
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2025-06-08 19:50:49 +0500

    feat: add middleware that redirects from home page to login page

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
